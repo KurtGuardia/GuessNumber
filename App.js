caa6fb18b25df98e948bd7c37632df761cdf4fb5
Kurt Guardia <kaguardiagamarra@wideagency.com>
2023-06-15 11:26:35 +0200

    created a reusable Title component

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import StartGameScreen from './screens/StartGameScreen'
 import { LinearGradient } from 'expo-linear-gradient'
 import { useState } from 'react'
 import GameScreen from './screens/GameScreen'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 export default function App() {
   const [userNumber, setUserNumber] = useState(null)
@@ -30,7 +31,9 @@ export default function App() {
         style={styles.rootScreen}
         imageStyle={styles.backgroundImage}
       >
-        {screen}
+        <SafeAreaView style={styles.rootScreen}>
+          {screen}
+        </SafeAreaView>
       </ImageBackground>
     </LinearGradient>
   )
--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,8 +1,24 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import Title from '../components/Title'
 
 export default function GameScreen() {
-  return <Text>GameScreen</Text>
+  return (
+    <View style={styles.screen}>
+      <Title>Opponent's Guess</Title>
+      {/* GUESS */}
+      <View>
+        <Text>Higher of lower?</Text>
+        {/* + -  */}
+      </View>
+      {/* <View>LOG ROUNDS</View> */}
+    </View>
+  )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 24,
+  },
+})
