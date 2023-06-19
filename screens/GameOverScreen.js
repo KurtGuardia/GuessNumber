cfc5ba4a19f066cb456a140622ec19ed2bcc9aca
Kurt Guardia <kaguardiagamarra@wideagency.com>
2023-06-19 11:06:01 +0200

    styled GmeOver screen

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,7 +1,7 @@
 import { Image, StyleSheet, Text, View } from 'react-native'
 import Title from '../components/UI/Title'
 import Colors from '../constants/colors'
-
+import PrimaryButton from '../components/UI/PrimaryButton'
 export default function GameOverScreen() {
   return (
     <View style={styles.rootConainer}>
@@ -12,7 +12,15 @@ export default function GameOverScreen() {
           style={styles.image}
         />
       </View>
-      <Text>Number of rounds: </Text>
+      <Text style={styles.summarText}>
+        Your phone needed{' '}
+        <Text style={styles.highlight}>3</Text> rounds to
+        guess the number{' '}
+        <Text style={styles.highlight}>8</Text>
+      </Text>
+      <PrimaryButton onPress={() => {}}>
+        Start New Game
+      </PrimaryButton>
     </View>
   )
 }
@@ -28,7 +36,7 @@ const styles = StyleSheet.create({
     width: 300,
     height: 300,
     borderRadius: 150,
-    borderColor: Colors.primary800,
+    borderColor: Colors.accent500,
     borderWidth: 3,
     overflow: 'hidden',
     margin: 36,
@@ -36,5 +44,15 @@ const styles = StyleSheet.create({
   image: {
     width: '100%',
     height: '100%',
+  },
+  summarText: {
+    fontFamily: 'open-sans',
+    fontSize: 24,
+    textAlign: 'center',
+    marginBottom: 24,
+  },
+  highlight: {
+    fontFamily: 'open-sans-bold',
+    color: Colors.primary500,
   },
 })
